894a8ded6103b94c7abeaf069a3fae3cee46c875
Abish DM <abish.dm122@gmail.com>
2025-07-22 22:53:33 +0530

    pattern 5 is added

---
+++ Pattern/Pattern5.java
@@ -0,0 +1,25 @@
+// Input: Enter the number = 6
+// Output:
+// 1 2 3 4 5 6
+// 1 2 3 4 5
+// 1 2 3 4
+// 1 2 3
+// 1 2 
+// 1
+
+import java.util.*;
+public class Pattern5 {
+    public static void main(String[] abish){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number= ");
+        int n=sc.nextInt(),c=1;
+        for (int i=n; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print(c+" ");
+                c++;
+            }
+            System.out.println();
+            c=1;
+        }
+    }
+}
